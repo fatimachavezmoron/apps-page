49e2d0f3086d1894510416fd00f2a38503cf1188
Fatima Chavez <fatimachavezmoron@gmail.com>
2023-08-03 22:14:23 +0200

    change logo img

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import {  NavLink } from "react-router-dom"
+import logo from '../../assets/logo1.jpeg'
 
 export const Navbar = () => {
   return (
     <>
     <div className="navContainer">
-    <img className="logo" src={'https://www.meme-arsenal.com/memes/2035815f36f8c34cd29aa650354f3275.jpg'}/>
+    <img className="logo" src={logo}/>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
