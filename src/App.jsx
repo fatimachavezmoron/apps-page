dc6f531d9844811e70b9a9848c3b344c29b6cecb
Fatima Chavez <fatimachavezmoron@gmail.com>
2023-08-03 19:39:33 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 // import HookApp from './HookApp'
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { AboutScreen } from './pages/AboutScreen';
-// import { ContactScreen } from './pages/ContactScreen';
 import { HomeScreen } from './pages/HomeScreen';
 import { Navbar } from './pages/components/Navbar';
 import { UserProvider } from './pages/context/UserProvider';
@@ -19,7 +18,6 @@ function App() {
           <Routes>
             <Route path='/' element={<HomeScreen />}></Route>
             <Route path='/movies' element={<AboutScreen />}></Route>
-            {/* <Route path='/contact' element={<ContactScreen />}></Route> */}
             <Route path='/weather' element={<WeatherApp />}></Route>
             <Route path='/*' element={<Navigate to='/'/>}></Route>
             <Route path='/user' element={<LoginScreen />}></Route>
